4c065f6b8f24061448b09c7c8164d02555380985
YatesyTea <39750168+YatesyTea@users.noreply.github.com>
2024-02-29 22:43:07 +0000

    init hono app

---
+++ src/index.ts
@@ -0,0 +1,34 @@
+import { Hono } from "hono";
+import { Database } from "bun:sqlite";
+
+const app = new Hono();
+const db = new Database("poems.sqlite", { readonly: true });
+
+function generateVal(){
+  const currentDate = new Date();
+  const day = currentDate.getUTCDate();
+  const month = currentDate.getUTCMonth() + 1; // Months are zero-based, so add 1
+  const year = currentDate.getUTCFullYear();
+
+  const seed = day + month + year;
+  const range = 13845;
+  const randomValue = (seed % range) + 1;
+  return randomValue;
+}
+
+type Poem = {
+  id: number;
+  title: string;
+  poem: string;
+  poet: string;
+  tags: string;
+};
+
+app.get("/poem", (c) => {
+  const val = generateVal();
+  const query = db.query(`SELECT * FROM poems WHERE id = ${val} `);
+  const payload = query.all() as Poem[];
+  return c.json(payload);
+});
+
+export default app;
