ed90c1f81fa6b48af2372952825dd3af42c49af1
YatesyTea <39750168+YatesyTea@users.noreply.github.com>
2024-03-27 21:23:47 +0000

    attempt to serve static files v1

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,13 @@
 import { Hono } from "hono";
+import { serveStatic } from "hono/bun";
 import { Database } from "bun:sqlite";
 
 const app = new Hono();
 const db = new Database("poems.db", { readonly: true });
+
+app.use("/favicon.ico", serveStatic({ path: "./assets/favicon.ico" }));
+app.use("/style.css", serveStatic({ path: "./style.css" }));
+app.use("/stooti.jpg", serveStatic({ path: "./assets/stooti.jpg" }));
 
 function generateVal() {
   const currentDate = new Date();
@@ -34,16 +39,20 @@ app.get("/", (c) => {
       <head>
         <title>Stooti's Random Poem of The Day</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="description" content="Poem of the day for my wonderful girlfriend Stuti Pachisia <3."></meta>
-        <link rel="icon" href="/public/favicon.ico" type="image/x-icon" />
-        <link rel="stylesheet" href="./public/style.css" />
+        <meta
+          name="description"
+          content="Poem of the day for my wonderful girlfriend Stuti Pachisia <3."
+        ></meta>
+        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+        <link rel="stylesheet" href="/style.css" />
       </head>
       <body>
         <main>
-          <img class="rounded-pic" src="/public/stooti.jpg" alt="stooti" />
+          <img class="rounded-pic" src="/stooti.jpg" alt="stooti" />
           <h1>Stooti's Random Poem of the Day</h1>
           <p>
-            Today's Date is {today.getUTCDate()}-{today.getUTCMonth() + 1}-{today.getUTCFullYear()}
+            Today's Date is {today.getUTCDate()}-{today.getUTCMonth() + 1}-
+            {today.getUTCFullYear()}
           </p>
           <div class="display">
             <h2>
@@ -51,7 +60,11 @@ app.get("/", (c) => {
             </h2>
             <h3>by {selectedData.poet}</h3>
             <p class="line-break-text">{selectedData.poem}</p>
-            {selectedData.tags && <div class="listbox-display"><p>Tags: {selectedData.tags}</p></div>}
+            {selectedData.tags && (
+              <div class="listbox-display">
+                <p>Tags: {selectedData.tags}</p>
+              </div>
+            )}
           </div>
         </main>
       </body>
