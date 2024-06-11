89f10ee3b8bfa57e547a7cea05f5ed8484626890
YatesyTea <39750168+YatesyTea@users.noreply.github.com>
2024-06-11 19:50:10 +0100

    add some safety and change the html content to remove quirks mode

--- src/index.tsx
+++ src/index.tsx
@@ -34,15 +34,15 @@ app.get("/", (c) => {
   const query = db.query(`SELECT * FROM poems WHERE id = ${val} `);
   const selectedData = query.get() as Poem;
   const today = new Date();
-  return c.html(
+  
+  // Generate the HTML as a string
+  const htmlContent = `
+    <!DOCTYPE html>
     <html lang="en">
       <head>
         <title>Stooti's Random Poem of The Day</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta
-          name="description"
-          content="Poem of the day for my wonderful girlfriend Stuti Pachisia <3."
-        ></meta>
+        <meta name="description" content="Poem of the day for my wonderful girlfriend Stuti Pachisia <3."></meta>
         <link rel="icon" href="/favicon.ico" type="image/x-icon" />
         <link rel="stylesheet" href="/style.css" />
       </head>
@@ -50,28 +50,21 @@ app.get("/", (c) => {
         <main class="centre-content">
           <img class="rounded-pic" src="/stooti.jpg" alt="stooti" />
           <h1>Stooti's Random Poem of the Day</h1>
-          <p>
-            Today's Date is {today.getUTCDate()}-{today.getUTCMonth() + 1}-
-            {today.getUTCFullYear()}
-          </p>
+          <p>Today's Date is ${today.getUTCDate()}-${today.getUTCMonth() + 1}-${today.getUTCFullYear()}</p>
           <div class="display centre-content">
-            <h2>
-              Poem #{selectedData.id} - {selectedData.title}
-            </h2>
-            <h3>by {selectedData.poet}</h3>
+            <h2>Poem #${selectedData.id} - ${selectedData.title}</h2>
+            <h3>by ${selectedData.poet}</h3>
             <div class="centre-content">
-              <p class="line-break-text">{selectedData.poem}</p>
+              <p class="line-break-text">${selectedData.poem}</p>
             </div>
-            {selectedData.tags && (
-              <div class="listbox-display">
-                <p>Tags: {selectedData.tags}</p>
-              </div>
-            )}
+            ${selectedData.tags ? `<div class="listbox-display"><p>Tags: ${selectedData.tags}</p></div>` : ''}
           </div>
         </main>
       </body>
     </html>
-  );
+  `;
+  
+  return c.html(htmlContent);
 });
 
 export default app;
